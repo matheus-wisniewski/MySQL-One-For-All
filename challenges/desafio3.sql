6a387e9b6b007b8e9235e71d990f9336a078fab1
matheus-wisniewski <maahwiski@gmail.com>
2023-05-29 12:34:08 -0300

    fix req  3
    changed the fun function at line 4 to FORMAT instead of ROUND because ROUND doesnt return a string and changed the GROUP BY and ORDER BY to the alias pessoa_usuaria agrouped and ordered instead of the original table user_name

--- challenges/desafio3.sql
+++ challenges/desafio3.sql
@@ -1,9 +1,9 @@
 SELECT 
 u.user_name pessoa_usuaria,
 COUNT(h.play_history) musicas_ouvidas,
-ROUND((SUM(s.song_duration)/60), 2) total_minutos
+FORMAT((SUM(s.song_duration)/60), 2) total_minutos
 FROM SpotifyClone.user u
 JOIN SpotifyClone.history h ON u.user_id = h.user_id
 JOIN SpotifyClone.song s ON h.play_history = s.song_id
-GROUP BY user_name
-ORDER BY user_name;
+GROUP BY pessoa_usuaria
+ORDER BY pessoa_usuaria;
